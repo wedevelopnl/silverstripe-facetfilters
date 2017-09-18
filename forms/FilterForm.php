59b74304fb37adc6608bc6de5ef46ac09be422dc
Rens Korswagen <rens@thewebmen.com>
2017-09-18 15:32:42 +0200

    filter

--- forms/FilterForm.php
+++ forms/FilterForm.php
@@ -6,10 +6,16 @@ class FilterForm extends Form {
         $fields = new FieldList();
 
         foreach ($filters as $filter) {
-            $fields->push($filter->getFormField());
+            if ($field = $filter->getFormField()) {
+                $fields->push($field);
+            }
         }
 
         $actions = new FieldList();
+
+        if ($controller->ShowSearchButton) {
+            $actions->push(FormAction::create('', 'Zoeken')->setAttribute('name', ''));
+        }
 
         parent::__construct($controller, $name, $fields, $actions);
 
